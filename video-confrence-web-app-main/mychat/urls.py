6c20da20820bfd0892ced0a423fb69347176911d
stromerthaha <73738532+stromerthaha@users.noreply.github.com>
2023-09-27 13:47:26 +0530

    Add files via upload

---
+++ video-confrence-web-app-main/mychat/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+from django.views.static import serve
+#from django.conf.urls import re_path as url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
